25750c8ea818034a96bd82e731a44ae9a8695c07
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2015-11-12 23:09:18 +0900

    first commit

---
+++ macchinetta-qs.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# Macchinetta Quick Start ver 1.0.1 2015.11.12
+# 
+# Usage:
+#   macchinetta-qs groupId artifactId version
+#
+# Description:
+#   A small script for making projects
+#
+
+USAGE='Usage: macchinetta-qs groupId artifactId version'
+
+if [ $# -ne 3 ]; then
+    echo "Error: Wrong number of arguments" >&2
+    echo "$USAGE" >&2
+    exit 1
+fi
+
+mvn archetype:generate -B -DarchetypeCatalog=http://repo.terasoluna.org/nexus/content/repositories/terasoluna-gfw-releases -DarchetypeGroupId=org.terasoluna.gfw.blank -DarchetypeArtifactId=terasoluna-gfw-multi-web-blank-mybatis3-archetype -DarchetypeVersion=5.0.1.RELEASE -DgroupId=$1 -DartifactId=$2 -Dversion=$3
